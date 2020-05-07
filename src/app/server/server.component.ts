6344b2601b19169bcaf4a407c50d06e93ba15f85
Gaurav Gupta <guptag@tcd.ie>
2020-05-07 22:00:24 +0530

    Data Bindings

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,10 @@ import {Component} from "@angular/core";
 })
 
 export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'Offline';
 
+  getServerStatus(){
+    return this.serverStatus;
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,35 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   templateUrl: './servers.component.html',
+  // templateUrl: '<app-server><app-server><app-server><app-server>',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowUser: boolean = false;
+  serevrCreationStatus: string = "No Server was created!!";
+  serverName: string = "";
+  constructor() {
+    setTimeout(()=>{
+      this.allowUser = true;
+    },2000);
+  }
+
 
   ngOnInit(): void {
   }
 
+  onCreateServe() {
+    this.serevrCreationStatus = "Server was Created!!";
+  }
+
+  onupdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+
 }
+
